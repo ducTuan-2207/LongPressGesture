18e1de2b7a01446a94af73d58bc76a36020fec74
DucTuan <tuanhypc02@gmail.com>
2024-05-29 22:35:59 +0700

    Initial Commit

---
+++ LongPressGesture/LongPressGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressGestureApp.swift
+//  LongPressGesture
+//
+//  Created by macOS on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LongPressGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
