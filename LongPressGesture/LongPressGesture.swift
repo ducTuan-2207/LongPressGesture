652629ae77e98280ec7762d3f067c0169594353d
DucTuan <tuanhypc02@gmail.com>
2024-05-31 22:12:24 +0700

    longPressGestureView

---
+++ LongPressGesture/LongPressGesture.swift
@@ -0,0 +1,20 @@
+//
+//  LongPressGesture.swift
+//  LongPressGesture
+//
+//  Created by macOS on 31/05/2024.
+//
+
+import SwiftUI
+
+struct LongPressGesture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LongPressGesture_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGesture()
+    }
+}
